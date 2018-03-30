bd16d4dafd13d5bd2baf3012463795b302a2fa73
maxxbartko <maxx.bartko@gmail.com>
2018-03-29 20:33:20 -0700

    Passed all tests

--- web/request-handler.js
+++ web/request-handler.js
@@ -1,31 +1,31 @@
 var path = require('path');
 var archive = require('../helpers/archive-helpers');
-var httpHelpers = require("./http-helpers");
+var httpHelpers = require('./http-helpers');
 // require more modules/folders here!
 let index = archive.paths.index;
 let loading = archive.paths.loading;
 
 exports.handleRequest = function (req, res) {
-  if (req.method === "GET"){
+  if (req.method === 'GET') {
     let url = req.url.slice(1);
     console.log(url);
-    if (url){
+    if (url) {
       archive.isUrlArchived(url, (data) => {
         console.log(data);
         if (data) {
-          console.log("200")
-          longUrl = archive.paths.archivedSites+'/'+url;
-          console.log(longUrl) 
+          console.log('200');
+          longUrl = archive.paths.archivedSites + '/' + url;
+          // console.log(longUrl) 
           httpHelpers.serveAssets(res, longUrl, (data) => { return data; });
         } else {
-         // if not
-         console.log("404 should happen")
+          // if not
+          console.log('404 should happen');
           
           httpHelpers.serveAssets(res, url, (data) => { return data; }, 404);
         }
       });
     } else {
-        httpHelpers.serveAssets(res, index, (data) => { return data; });
+      httpHelpers.serveAssets(res, index, (data) => { return data; });
     }
     
     // archive.readListOfUrls((data) => {console.log(data)});
@@ -43,9 +43,9 @@ exports.handleRequest = function (req, res) {
       if (data) {
         httpHelpers.serveAssets(res, body, (data) => { return data; });
       } else {
-       // if not
-       archive.addUrlToList(body, (data) => { return data });
-       httpHelpers.serveAssets(res, loading, (data) => { return data; }, 302);
+        // if not
+        archive.addUrlToList(body, (data) => { return data; });
+        httpHelpers.serveAssets(res, loading, (data) => { return data; }, 302);
       }
     });
     // append to sites.txt
